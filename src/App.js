e78a0d6e06d7dfbdf490833da06f5e37aa110cfa
Power <testpowerbi055@gmail.com>
2025-12-04 11:12:53 +0700

    add secure v10

--- src/App.js
+++ src/App.js
@@ -355,7 +355,7 @@ const RepairForm = () => {
       }
 
       // ส่งข้อมูลจริง
-      const response = await fetch(API_URL, {
+      const response = await fetch(apiUrl, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
