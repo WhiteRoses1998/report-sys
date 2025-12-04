50faa43fed84a9f2c5373d095653291a6069fefa
Power <testpowerbi055@gmail.com>
2025-12-04 11:18:54 +0700

    add secure v11

--- src/App.js
+++ src/App.js
@@ -360,7 +360,7 @@ const RepairForm = () => {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(data),
+        body: JSON.stringify(dataToSubmit),
       })
         .then((res) => res.text())
         .then((text) => {
